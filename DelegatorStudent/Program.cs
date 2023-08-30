48143d089c54bf70553a441caa35207960efa941
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-08-30 12:06:50 +0900

    델리게이터와 람다를 이용해서 다양한 출력 구현

--- DelegatorStudent/Program.cs
+++ DelegatorStudent/Program.cs
@@ -8,8 +8,29 @@ namespace DelegatorStudent
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Students students = new Students();
+            students.Add(new Student("박나경", 4));
+            students.Add(new Student("김태연", 4));
+            students.Add(new Student("김하늘", 4));
+
+            students.Print();
+            students.Print((student) =>
+            {
+                Console.WriteLine("*********");
+                Console.WriteLine(" 이름 : " + student.Name);
+                Console.WriteLine(" 학점 : " + student.Score);
+            });
+            students.Print((s) =>
+            {
+                Console.WriteLine("<student>");
+                Console.WriteLine("  <name>" + s.Name + "</name>");
+                Console.WriteLine("  <score>" + s.Score + "</score>");
+                Console.WriteLine("</student>");
+            });
+            Console.WriteLine("</student>");
         }
     }
 }
