e495913896244b72c3c3b97e63c4e1d140260edd
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-08-30 11:59:43 +0900

    델리게이터를 정의한 Students 클래스 작성

--- DelegatorStudent/Student.cs
+++ DelegatorStudent/Student.cs
@@ -8,5 +8,16 @@ namespace DelegatorStudent
 {
     internal class Student
     {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public Student(string name, double score) { 
+            Name = name;
+            Score = score;
+        }
+
+        public override string ToString()
+        {
+            return Name + " : " + Score;
+        }
     }
 }
